200b5c3106429286e83befe27cf57dd7c91affaf
paultang <paultang@tencent.com>
2025-01-22 16:49:59 +0800

    Fix some compilation errors on Windows.

--- src/pbrt/util/error.cpp
+++ src/pbrt/util/error.cpp
@@ -72,7 +72,8 @@ void ErrorExit(const FileLoc *loc, const char *message) {
 #ifdef PBRT_IS_OSX
     exit(1);
 #else
-    std::quick_exit(1);
+    //std::quick_exit(1);
+    std::exit(1);
 #endif
 }
 
